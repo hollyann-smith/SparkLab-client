082dd4cfb019c621ddc9e7af05ee912271dfbc4a
Holly Smith <hollyschuldt@gmail.com>
2024-08-08 21:55:34 -0500

    user auth and basic routing set up

---
+++ pages/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>Generate a random spark of creativity!
+
+      --Load a random idea card--
+
+      --button to generate another random idea--
+
+      --button to explore on your own--
+      (this routes to explore ideas page)
+    </div>
+  );
+}
---
+++ pages/ideas/new.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function AddIdea() {
+  return (
+    <div>**Create new idea form**   </div>
+  );
+}
